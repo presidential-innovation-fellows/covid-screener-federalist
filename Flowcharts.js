9f79a1b3fe6cedc4e313836009c74926da59507b
Irtaza Barlas <irtaza.barlas@gsa.gov>
2021-03-06 09:28:28 -0500

    2.4.4 made the modified text for Click here.

--- Flowcharts.js
+++ Flowcharts.js
@@ -553,7 +553,7 @@ const flowcharts = [
                 "text": `<span style='font-size:2em; line-height: 1.2em;'>Access to VA Facilities not approved</span>
                         <hr>Valid for $DATETIME.
                         <hr tabindex = "-1" >Contact your supervisor for further instructions.
-                        <a href="https://www.cdc.gov/screening/further-instructions.html" target="_blank" title="Click Here">Click Here for Further Instructions</a>`,
+                        <a href="https://www.cdc.gov/screening/further-instructions.html" target="_blank" title="Click Here">Click Here for CDC Guidelines.</a>`,
                 "sms" : "",
                 "info": "",
                 "next": ""
